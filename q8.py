eb7ac5f236b7329a8efa04f2f73baaa7325cb393
Venusstars1 <nkohavi2028@kehillahstudent.org>
2025-02-20 11:01:47 -0800

    finished

--- q8.py
+++ q8.py
@@ -6,5 +6,12 @@ ends with one of these letters: p, t, n
 
 (This should print out 18 words in total)
 """
-
-# YOUR CODE HERE
+import random
+for i in range (18):
+    start = ["c", "t", "b"]
+    start_r = random.choice(start)
+    middle = ["a", "o"]
+    middle_r = random.choice(middle)
+    end = ["p", "t", "n"]
+    end_r = random.choice(end)
+    print(start_r, middle_r, end_r)
